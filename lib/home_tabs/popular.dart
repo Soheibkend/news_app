fd233a7e63d5dc09b8f56a9ca90f443f91f293a0
Soheibkend <soheibkenden@gmail.com>
2022-03-14 14:23:18 +0100

    add popular page

--- lib/home_tabs/popular.dart
+++ lib/home_tabs/popular.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lorem_ipsum/lorem_ipsum.dart';
+
 
 class Popular extends StatefulWidget {
   const Popular({Key? key}) : super(key: key);
@@ -10,6 +12,56 @@ class Popular extends StatefulWidget {
 class _PopularState extends State<Popular> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(itemBuilder: (context, index) {
+      return _drawSingleRow();
+    },
+    itemCount: 20,);
+  }
+
+  Widget _drawSingleRow () {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 110,
+            height: 110,
+            child: Container(color: Colors.black,),
+          ),
+          SizedBox(width: 10,),
+          Expanded(
+            child: Column(
+              children: [
+                Text(loremIpsum(words: 6),
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 29,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("kendjou soheib",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black45
+                      ),),
+                    Text("10 min",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black45
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+
+      ),
+    );
   }
 }
