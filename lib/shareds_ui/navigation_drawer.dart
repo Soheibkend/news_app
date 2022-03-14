5c9612a915c09218e26517000dcf7a8447f14171
Soheibkend <soheibkenden@gmail.com>
2022-03-14 21:16:50 +0100

    add navogation menu

--- lib/shareds_ui/navigation_drawer.dart
+++ lib/shareds_ui/navigation_drawer.dart
@@ -7,8 +7,38 @@ class NavigationDrawer extends StatefulWidget {
 }
 
 class _NavigationDrawerState extends State<NavigationDrawer> {
+
+  List<String> navMenu = [
+    'Explore',
+    'Headline News',
+    'Read Later',
+    'Videos',
+    'Photos',
+    'Settings',
+    'Logout'
+  ];
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 8, top: 75),
+          child: ListView.builder(itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(navMenu[index],
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black54
+              ),),
+              trailing: Icon(Icons.chevron_right,
+              color: Colors.grey,),
+              onTap: (){
+
+              },
+            );
+          },
+          itemCount: navMenu.length,),
+        )
+    );
   }
 }
